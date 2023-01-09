245d050a0b3d9bd8265296d82c54ceab05202e54
Jefferson Carvalho <68786776+myjefferson@users.noreply.github.com>
2023-01-09 11:24:08 -0300

    Update general_functions.js

--- assets/js/functions_edityme/general_functions.js
+++ assets/js/functions_edityme/general_functions.js
@@ -228,8 +228,8 @@ function salvarImagem(){
 
     html2canvas($("#edit-desk")[0]).then(function(canvas) {
         $(".pagina-principal").append(`<a class='download_image' download='EditMe_Image_${idRandom()}.jpg' href="${canvas.toDataURL()}">Baixar</a>`)
-        $(".btn_download_image")[0].click()
-        $(".btn_download_image").remove();
+        $(".btn-download-image")[0].click()
+        $(".btn-download-image").remove();
     });
 
     //Seleciona a imagem novamente
