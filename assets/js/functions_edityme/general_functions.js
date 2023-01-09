f0d7a812a4f969a1c1b07c3290c773adeeda503a
Jefferson Carvalho <68786776+myjefferson@users.noreply.github.com>
2023-01-09 11:22:08 -0300

    Correção do botão de Baixar Imagem
    Corrigindo consulta jQuery no botão de download de imagem

--- assets/js/functions_edityme/general_functions.js
+++ assets/js/functions_edityme/general_functions.js
@@ -228,8 +228,8 @@ function salvarImagem(){
 
     html2canvas($("#edit-desk")[0]).then(function(canvas) {
         $(".pagina-principal").append(`<a class='download_image' download='EditMe_Image_${idRandom()}.jpg' href="${canvas.toDataURL()}">Baixar</a>`)
-        $(".download_image")[0].click()
-        $(".download_image").remove();
+        $(".btn_download_image")[0].click()
+        $(".btn_download_image").remove();
     });
 
     //Seleciona a imagem novamente
